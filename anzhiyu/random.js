0d875af4e285d72c8116caa377483e8815679a41
Edison <ansonyean626@gmail.com>
2024-08-30 15:22:48 +0800

    Site updated: 2024-08-30 15:22:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/31/hello-world/","2024/07/31/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2024/07/31/hexo/","2024/07/31/markdown/","2024/08/30/week3/","2024/08/30/week2/","2024/08/30/week1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
